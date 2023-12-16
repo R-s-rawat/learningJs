75cf314e8a608ba6bf00ce31f316a5c808aea324
unknown <rohitrawat590590@gmail.com>
2023-12-16 22:24:07 +0530

    todo#

---
+++ one.js
@@ -0,0 +1,23 @@
+let nae = "rohit";
+
+
+let v = "rawat";
+
+dfjjlsdf = falkjdf;
+let jdfalsd = sdjf;
+
+if (dfjjlsdf == falkjdf) {
+  printf("");
+}
+
+rohit;
+
+let name = "rohit";
+let undefined = sdjf;;
+
+let object = ajf;
+
+let = Dflk;
+var names = ajsldfj;
+
+
